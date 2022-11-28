b1a4508a6e7972e338b9e61b3ea4a2598bb3918f
edelvarden <edelvarden@gmail.com>
2022-11-28 20:31:34 +0200

    fix winget install

--- 2_install_winget.ps1
+++ 2_install_winget.ps1
@@ -4,13 +4,14 @@
 $localPackages = @(
     "Microsoft.VCLibs.x64.14.00.Desktop.appx"   # source: https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx
     "Microsoft.UI.Xaml.2.7.appx"                # source: https://www.nuget.org/packages/Microsoft.UI.Xaml/2.7.0
-    "Microsoft.DesktopAppInstaller.msixbundle"  # source: https://github.com/microsoft/winget-cli/releases/
-    "Microsoft.WebpImageExtension.appx"
 )
 foreach ($package in $localPackages) {
     Write-Output "Install local package $package"
     Add-AppxPackage -path ".\packages\$package" -ForceUpdateFromAnyVersion
 }
+
+# source: https://github.com/microsoft/winget-cli/releases/
+Add-AppxProvisionedPackage -Online -PackagePath ".\packages\Microsoft.DesktopAppInstaller.msixbundle" -LicensePath ".\packages\Microsoft.DesktopAppInstaller_License.xml"
 
 # ---------------------------------------
 # install & update from MS Store 
@@ -22,5 +23,5 @@ $msstorePackages = @(
 )
 foreach ($package in $msstorePackages) {
     Write-Output "Install MS Store package $package";
-    winget install $package -s msstore --accept-package-agreements --silent
+    winget install $package -s msstore --accept-source-agreements --accept-package-agreements --silent
 }
